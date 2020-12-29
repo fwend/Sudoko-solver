63efdba5f00fb840a8e3e4b32f59fe4f1b7e4c0e
fwend <josvanuden@gmail.com>
2020-12-29 02:06:17 +0100

    add comments

--- main.js
+++ main.js
@@ -5,37 +5,48 @@ function sudoku(grid) {
     const subh = 3;
 
     function solve(pos) {
-        if (pos === total)
+        if (pos === total) {
             return true;
+        }
 
         const col = pos % base;
         const row = Math.floor(pos / base);
 
-        if (grid[row][col] !== 0)
+        if (grid[row][col] !== 0) {
             return solve(pos + 1);
+        }
 
         outer:
         for (let n = 1; n < base + 1; n++) {
-            for (let i = 0; i < base; i++)
-                if (grid[i][col] === n || grid[row][i] === n)
+            // check the columns and rows
+            for (let i = 0; i < base; i++) {
+                if (grid[i][col] === n || grid[row][i] === n) {
                     continue outer;
+                }
+            }
 
+            // check the subsquares
             const sc = Math.floor(col / subw) * subw;
             const sr = Math.floor(row / subh) * subh;
-            for (let r = sr; r < sr + subh; r++)
-                for (let c = sc; c < sc + subw; c++)
-                    if (grid[r][c] === n)
+            for (let r = sr; r < sr + subh; r++) {
+                for (let c = sc; c < sc + subw; c++) {
+                    if (grid[r][c] === n) {
                         continue outer;
+                    }
+                }
+            }
 
             grid[row][col] = n;
-            if (solve(pos + 1))
+            if (solve(pos + 1)) {
                 return true;
-            grid[row][col] = 0;
+            }
+            grid[row][col] = 0;  // backtrack
         }
         return false;
     }
 
-    if (!solve(0))
+    if (!solve(0)) {
         return "no solution found";
+    }
     return grid;
 }
